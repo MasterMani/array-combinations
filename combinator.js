38190788d10d3310169118c42fbef9790d7d730c
MasterMani <sabarinathan305@gmail.com>
2015-12-07 17:42:30 +0530

    create combinator.js

---
+++ combinator.js
@@ -0,0 +1,34 @@
+function combine(arr1, arr2, combineParameter){
+	var newArr = [];
+	arr1.forEach(function(elm1, idx1){
+		arr2.forEach(function(elm2, idx2){
+			newArr.push(elm1 + combineParameter + elm2)
+		})
+	});
+	return newArr
+}
+
+function recursiveCombine(inputArray, combineParameter){
+	var parentArray = inputArray,
+		parArrLen = parentArray.length;
+	if(parArrLen > 1){
+		var combinedArr = combine(parentArray[parArrLen-1], parentArray[parArrLen-2], combineParameter);
+		parentArray.splice(parArrLen-2, 2, combinedArr)
+		recursiveCombine(parentArray, combineParameter);
+	}
+	return parentArray[0]
+}
+
+/* Example
+
+var ar1 = [1,2,3],
+ar2 = ['a','b','c'],
+ar3 = ["x", "y", "z"],
+ar4 = ["mani", "sabari", "fa"],
+ar5 = ["3", "4", "5"],
+parentArray = [ar1, ar2, ar3, ar4, ar5],
+x = recrusiveCombine(parentArray, "_|_");
+
+console.log(x);
+
+*/
