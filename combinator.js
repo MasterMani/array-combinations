4f4246993bb5c7c2685d736f66edac0bcf15edfb
MasterMani <sabarinathan305@gmail.com>
2017-07-14 10:27:19 +0530

    changing function name

--- combinator.js
+++ combinator.js
@@ -27,7 +27,7 @@ ar3 = ["x", "y", "z"],
 ar4 = ["mani", "sabari", "fa"],
 ar5 = ["3", "4", "5"],
 parentArray = [ar1, ar2, ar3, ar4, ar5],
-x = recrusiveCombine(parentArray, "_|_");
+x = recursiveCombine(parentArray, "_|_");
 
 console.log(x);
 
